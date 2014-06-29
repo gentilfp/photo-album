934a5cfc8a576ea2e86070199ef0e7475ab8f84b
Felipe Gentil <cdigentil@gmail.com>
2014-06-29 18:59:03 -0300

    Added comments to photos using js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,14 @@ class CommentsController < ApplicationController
 
   def create
   	@photo = Photo.get(params[:comment][:photo_id])
-    # binding.pry
   	@photo.comments << Comment.new(params[:comment])
 
     if @photo.comments.save
   		respond_to do |format|
   			format.html { redirect_to @photo }
+        format.js
   		end
-  	else
-  		redirect_to @photo
-  	end
+    end
   end
 
 end
